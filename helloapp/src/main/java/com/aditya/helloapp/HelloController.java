82214b39ddb9741d491ec3c99f6fc31b05ce41d0
2472814 <2472814@LTIN690015.cts.com>
2026-02-20 11:47:20 +0530

    Created post api using postmapping

--- helloapp/src/main/java/com/aditya/helloapp/HelloController.java
+++ helloapp/src/main/java/com/aditya/helloapp/HelloController.java
@@ -1,14 +1,21 @@
 package com.aditya.helloapp;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 public class HelloController {
 
-	@GetMapping("/greet/{name}")
-	public String greet(@PathVariable String name) {
+	/*
+	 * @GetMapping("/greet/{name}") public String greet(@PathVariable String name) {
+	 * return "Hello"+ name; }
+	 */
+	
+	@PostMapping("/welcome")
+	public String welcome(@RequestBody String name) {
 		return "Hello"+ name;
 	}
-
 }
